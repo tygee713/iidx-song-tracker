c6e2af7b7f1872fa1b1d1e82b33bdeb83d939732
Tyler Geerdts <tygee713@gmail.com>
2022-11-21 12:06:04 -0500

    added register button

--- imports/ui/LoginForm.svelte
+++ imports/ui/LoginForm.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Meteor } from 'meteor/meteor'
+  import { Accounts } from 'meteor/accounts-base'
 
   let username = ''
   let password = ''
@@ -7,10 +8,14 @@
   const handleSubmit = () => {
     Meteor.loginWithPassword(username, password)
   }
+  const handleRegister = () => {
+    Accounts.createUser(username, password)
+  }
 </script>
 
 <form class="form-group navbar-form" on:submit|preventDefault={handleSubmit}>
     <input class="form-control" type="text" placeholder="Username" name="username" required bind:value={username} />
     <input class="form-control" type="text" placeholder="Password" name="password" required bind:value={password} />
     <button class="btn btn-default" type="submit">Log In</button>
+    <button class="btn btn-default" on:click={handleRegister}>Register</button>
 </form>
